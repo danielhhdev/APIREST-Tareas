bc26747c9c12484bc52f902cc1e7e96a626eb820
Daniel Hernandez <daniel.hhuerta@gmail.com>
2022-09-06 19:02:32 +0200

    Primer commit

---
+++ src/main/java/com/danielhh/APIRESTTareas/ApirestTareasApplication.java
@@ -0,0 +1,13 @@
+package com.danielhh.APIRESTTareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestTareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielhh/APIRESTTareas/ApirestTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielhh.APIRESTTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
