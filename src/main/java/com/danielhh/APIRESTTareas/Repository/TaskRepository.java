24e3ed99c8796e4296a5dd1fbcdfce369edf1789
Daniel Hernandez <daniel.hhuerta@gmail.com>
2022-09-16 14:20:52 +0200

    commit

--- src/main/java/com/danielhh/APIRESTTareas/Repository/TaskRepository.java
+++ src/main/java/com/danielhh/APIRESTTareas/Repository/TaskRepository.java
@@ -3,6 +3,9 @@ package com.danielhh.APIRESTTareas.Repository;
 import com.danielhh.APIRESTTareas.Model.Task;
 import com.danielhh.APIRESTTareas.Model.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,4 +17,16 @@ public interface TaskRepository extends JpaRepository <Task, Long> {
     Creamos un método que nos devuelva una lista de las tareas en funcion de un estado
      */
     public List <Task> findAllByTaskStatus (TaskStatus taskStatus);
+
+
+    /*
+    Marca una tarea como finalizada
+    Se hace una query nativa, para que cuando se llame a ese metodo pueda hacer la modificacion en la BD
+    @Modifying hace que esta query es una query de actualizacion
+     */
+    @Modifying
+    @Query(value = "UPDATE TASK SET FINISHED = true WHERE ID=id", nativeQuery = true)
+    public  void markTaskAsFinished(@Param("id")Long id);
+
+
 }
